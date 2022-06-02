6b89bd43c6bf9a5334e3ca65a43c7066fecbf3df
mero2online <mero2online@gmail.com>
2022-06-02 08:25:36 +0200

    EDIT: Build_App to onefile

--- Build_App.py
+++ Build_App.py
@@ -1,11 +1,23 @@
 import PyInstaller.__main__
+import os
+import shutil
+
+cwd = os.getcwd()
+wd = f'{cwd}\\dist\\LAS_Handler'
+if os.path.exists(wd):
+    shutil.rmtree(wd)
 
 PyInstaller.__main__.run([
     'LAS_Handler.py',
-    '--onefile',
+    # '--onefile',
     '--windowed',
     '--add-data', 'src;src',
     '-i', ".\src\las.ico",
     '--splash', ".\src\las.png",
     '--exclude-module', 'matplotlib'
 ])
+
+os.system(f'start {cwd}\\dist\\LAS_Handler_Setup_Generator.iss')
+
+os.chdir(f'{cwd}\dist')  # Change directory to run command
+os.system('start.')  # Run command
